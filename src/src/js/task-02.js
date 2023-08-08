de25f0b685d1ee130eb18f421825081627672a00
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-08 23:02:54 +0200

    exe2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul#ingredients")
+
+ingredients.forEach(vegetable => {
+  const lastItem = document.createElement("li");
+  lastItem.textContent = vegetable;
+  list.append(lastItem);
+})
+
+
