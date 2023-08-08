8a0f53c7634ca36ffdd62d1f2a0c13df5663d119
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-08 22:40:35 +0200

    f

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,3 +1,10 @@
-let a = 2;
-let body2 = document.querySelectorAll('li')
-console.log(body2);
+const category = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${category.length}`);
+
+category.forEach(foo => {
+    let name = foo.firstElementChild.innerHTML;
+    let number = foo.querySelectorAll("li").length
+
+    console.log(`Category: ${name} Elements: ${number}`);
+});
