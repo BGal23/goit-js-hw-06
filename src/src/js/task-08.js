130ecf061ce18c3fab799763228d8fad19e9d840
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-12 09:29:43 +0200

    8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,11 +1,16 @@
-const form = document.querySelector("form")
-
+const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
-
     if (form.email.value.length === 0 || form.password.value.length === 0) {
         return alert("Complete the missing fields")
+    }
+    else {
+        let user = {};
+        user.email = form.email.value;
+        user.password = form.password.value;
+        console.log(user);
+        event.currentTarget.reset();
     }
 }
